632c3e9e211b6c12830e5b5c05ce0130b251a2ab
buritto <ybiz177@gmail.com>
2017-11-09 23:15:42 +0500

    Review 2

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -19,88 +19,19 @@ namespace WindowsFormsApp1
         {
             graphics.DrawString(count.ToString(), font1, Brushes.Black,rec);
             graphics.DrawRectangle(Pens.Red, rec);
-        }
+        }   
 
-        public void DrawSquareSpiral(int count)
-        {
-            var circle = new CircularCloudLayouter(center);
-            var rectangleSize = new Size(50, 50);
-            Graphics graphics = this.CreateGraphics();
-            graphics.Clear(Color.WhiteSmoke);
-            for (int i = 0; i < count; i++)
-            {
-                var rec = circle.PutNextRectangle(rectangleSize);
-                graphics.DrawString(circle.CountTags.ToString(), font1, Brushes.Black, rec);
-                graphics.DrawRectangle(Pens.Red, rec);
-            }
-        }
-
-        public void DrawLargeIntersectionTest()
-        {
-            var circle = new CircularCloudLayouter(center);
-            Graphics graphics = this.CreateGraphics();
-            graphics.Clear(Color.WhiteSmoke);
-            var sizeCenterRectangle = new Size(150, 150);
-            var sizeLittleRectangleAroundCenter = new Size(50, 50);
-            var sizeBigRectangle = new Size(300, 150);
-            DrawRectangle(circle.PutNextRectangle(sizeCenterRectangle), graphics, circle.CountTags);
-            for (int i = 0; i < 6; i++)
-            {
-                DrawRectangle(circle.PutNextRectangle(sizeLittleRectangleAroundCenter), graphics, circle.CountTags);
-            }
-            DrawRectangle(circle.PutNextRectangle(sizeBigRectangle), graphics, circle.CountTags);
-        }
-
-
-        public void DrawSpiralWithBigSquareInCenterAndLittleSquareAround()
-        {
-            var circle = new CircularCloudLayouter(center);
-            var centerSqareSize = new Size(150, 150);
-            Graphics graphics = this.CreateGraphics();
-            graphics.Clear(Color.WhiteSmoke);
-            DrawRectangle(circle.PutNextRectangle(centerSqareSize), graphics, circle.CountTags);
-            var littleSqareSize = new Size(50, 50);
-            for (int i = 0; i < 51; i++)
-            {
-                DrawRectangle(circle.PutNextRectangle(littleSqareSize), graphics, circle.CountTags);
-            }
-
-        }
-
-        public void DrawDifferentRectangle()
-        {
-            var circle = new CircularCloudLayouter(center);
-            var sizeRectangles = new List<Size>()
-            {
-                new Size(50, 50),
-                new Size(100, 100),
-                new Size(50, 50),
-                new Size(150, 50),
-                new Size(100, 150),
-                new Size(50, 100),
-                new Size(100, 50),
-                new Size(150, 50),
-                new Size(50, 100),
-            };
-            Graphics graphics = this.CreateGraphics();
-            graphics.Clear(Color.WhiteSmoke);
-            foreach (var sizeRectangle in sizeRectangles)
-            {
-                DrawRectangle(circle.PutNextRectangle(sizeRectangle), graphics, circle.CountTags);
-            }
-
-        }
-
+        private Random rnd = new Random();
         private CircularCloudLayouter cloudTag = new CircularCloudLayouter(new Point(400, 300));
+        private int cl;
         public void drawRectangle(int width, int height)
         {
-            
+            cl++;
             var size = new Size(width, height);
             var rec = cloudTag.PutNextRectangle(size);
             Graphics graphics = this.CreateGraphics();
             graphics.DrawRectangle(Pens.Red, rec);
-            graphics.DrawString(cloudTag.CountTags.ToString(), font1, Brushes.Black, rec);
-
+            graphics.DrawString(cl.ToString(), font1, Brushes.Black, rec);
         }
 
         public MyForm(int width, int height)
@@ -126,6 +57,13 @@ namespace WindowsFormsApp1
                 Size = new Size(50, 50),
                 Text = "Add"
             };
+
+            var buttonAddRandom = new Button
+            {
+                Location = new Point(10, 120),
+                Size = new Size(100, 100),
+                Text = "AddRandom"
+            };
             var buttonClear = new Button
             {
                 Location = new Point(60, 60),
@@ -133,44 +71,10 @@ namespace WindowsFormsApp1
                 Text = "Clear",
             };
 
-
-            var buttonDrawSquareSpiral = new Button
-            {
-                Location = new Point(10, 120),
-                Size = new Size(80, 60),
-                Text = "DrawSquareSpiral ",
-            };
-
-            var buttonLargeIntersection = new Button
-            {
-                Location = new Point(10, 180),
-                Size = new Size(80, 60),
-                Text = "DrawLargeIntersectionTest",
-            };
-
-            var buttonhBigSquareInCenter = new Button
-            {
-                Location = new Point(10, 240),
-                Size = new Size(80, 60),
-                Text = "DrawBigSquareInCenter",
-            };
-
-            var buttonhDifferentRectangle = new Button
-            {
-                Location = new Point(10, 300),
-                Size = new Size(80, 60),
-                Text = "DrawDifferentRectangle",
-            };
-
-
-
-
             buttonAdd.Click += (sender, args) => drawRectangle(int.Parse(textBoxWidh.Text),
                 Int32.Parse(textBoxHeight.Text));
-            buttonDrawSquareSpiral.Click += (sender, args) => DrawSquareSpiral(23);
-            buttonLargeIntersection.Click += (sender, args) => DrawLargeIntersectionTest();
-            buttonhBigSquareInCenter.Click += (sender, args) => DrawSpiralWithBigSquareInCenterAndLittleSquareAround();
-            buttonhDifferentRectangle.Click += (sender, args) => DrawDifferentRectangle();
+
+            buttonAddRandom.Click += (sender, args) => drawRectangle(rnd.Next(10, 100), rnd.Next(10, 100));
             buttonClear.Click += (sender, args) =>
             {
                 Graphics graphics = this.CreateGraphics();
@@ -179,14 +83,11 @@ namespace WindowsFormsApp1
 
             };
 
-
-            Controls.Add(buttonLargeIntersection);
+            Controls.Add(buttonAddRandom);
             Controls.Add(textBoxHeight);
             Controls.Add(textBoxWidh);
             Controls.Add(buttonAdd);
-            Controls.Add(buttonDrawSquareSpiral);
-            Controls.Add(buttonhBigSquareInCenter);
-            Controls.Add(buttonhDifferentRectangle);
+
             Controls.Add(buttonClear);
         }
     }
--- WindowsFormsApp1/TagsCloud.cs
+++ WindowsFormsApp1/TagsCloud.cs
@@ -9,322 +9,135 @@ using NUnit.Framework;
 
 namespace WindowsFormsApp1
 {
+
+    [TestFixture]
+    public class TestClass
+    {
+        public Point center = new Point(100, 100);
+
+
+        [Test]
+        public void BuildRectangleInCenter()
+        {
+            var c = new CircularCloudLayouter(center);
+            var sizeRectangle = new Size(10, 10);
+
+            var actualRectangle = c.PutNextRectangle(sizeRectangle);
+            var expceptedRectangle = new Rectangle(new Point(center.X + 5, center.Y + 5), sizeRectangle);
+
+            Assert.AreEqual(expceptedRectangle, actualRectangle);
+        }
+
+
+        public Rectangle BuildRectangle(Size recSize, double angle)
+        {
+            var x = angle * Math.Cos(angle) + center.X + recSize.Width / 2;
+            var y = angle * Math.Sin(angle) + center.Y + recSize.Height / 2;
+            return new Rectangle(new Point((int) x, (int) y), recSize);
+        }
+
+        [TestCase(10, 10, 20)]
+        [TestCase(1, 1, 40)]
+        [TestCase(10, 5, 20)]
+        [TestCase(5 , 10, 20)]
+        public void BuildSameRectangle(int width,int height , int countRectangle)
+        {
+            var c = new CircularCloudLayouter(center);
+            var sizeRectangle = new Size(width, height);
+            var setExpectedRectangle = new List<Rectangle>();
+            var step = 0.1;
+
+            for (var angle = 0.0; angle < countRectangle * countRectangle; angle += step)
+            {
+
+                setExpectedRectangle.Add(BuildRectangle(sizeRectangle, angle));
+            }
+
+            var setActualRectangle = new List<Rectangle>();
+
+            for (int i = 0; i < countRectangle; i++)
+            {
+                setActualRectangle.Add(c.PutNextRectangle(sizeRectangle));
+            }
+
+            setActualRectangle.Distinct().Should().BeSubsetOf(setExpectedRectangle.Distinct());
+            setActualRectangle.Distinct().Count().Should().Be(countRectangle);
+        }
+
+        [TestCase(100, 5)]
+        [TestCase(100, 25)]
+        [TestCase(100, 1)]
+        [TestCase(100, 50)]
+        [TestCase(100, 80)]
+        public void BuildDifferentRectangle(int countRectangle, int countDifferentRepresentative)
+        {
+            var setExpectedRectangles = new List<Rectangle>();
+            var setActualRectangles = new List<Rectangle>();
+            var c = new CircularCloudLayouter(center);
+
+            var rnd = new Random();
+            for (int representative = 0; representative < countDifferentRepresentative; representative++)
+            {
+                var width  = rnd.Next(10, 100);
+                var height = rnd.Next(10, 100);
+                for (int i = 0; i < countRectangle / countDifferentRepresentative; i++)
+                {
+                    setActualRectangles.Add(c.PutNextRectangle(new Size(width, height)));
+                }
+
+                var step = 0.1;
+
+                for (var angle = 0.0; angle < countRectangle * countRectangle; angle += step)
+                {
+
+                   setExpectedRectangles.Add(BuildRectangle(new Size(width, height), angle));
+                }
+
+            }
+            setActualRectangles.Distinct().Should().BeSubsetOf(setExpectedRectangles.Distinct());
+            setActualRectangles.Distinct().Count().Should().Be(countRectangle/ countDifferentRepresentative * countDifferentRepresentative);
+
+        }
+
+    }
+
     public class CircularCloudLayouter
     {
-        public Point CenterTagsCload;
-        public int CountTags;
-        private Point center;
-        private int countTagsBeforeTurning ;
-        private int countTurn;
-        private List<Rectangle> allBuildRectangles;
-        public Point DirectionSpiral = WindowsFormsApp1.Direction.None;
-        
+        public Point centerCloud;
+
+        public List<Rectangle> BuiltRectangles;
+
+        private double step;
 
         public CircularCloudLayouter(Point center)
         {
-            CenterTagsCload = center;
-            this.center = new Point(center.X, center.Y);
-            allBuildRectangles = new List<Rectangle>();
-        }
-
-        private void CountingTagsBeforeTurning()
-        {
-            countTagsBeforeTurning = countTurn / 2 ;
-        }
-
-        private void ChangeDirection()
-        {
-            if (DirectionSpiral == Direction.None)
-            {
-                DirectionSpiral = Direction.Left;
-                return;
-            }
-            if (DirectionSpiral == Direction.Left)
-            {
-                DirectionSpiral = Direction.Down;
-                return;
-            }
-            if (DirectionSpiral == Direction.Down)
-            {
-                DirectionSpiral = Direction.Rigth;
-                return;
-            }
-
-            if (DirectionSpiral == Direction.Rigth)
-            {
-                DirectionSpiral = Direction.Top;
-                return;
-            }
-            DirectionSpiral = Direction.Left;
-        }
-
-        private void AnalyzeLayout()
-        {
-            if (countTagsBeforeTurning == 0)
-            {
-                ChangeDirection();
-                CountingTagsBeforeTurning();
-                countTurn++;
-                return;;
-            }
-            countTagsBeforeTurning--;
-        }
-
-        private void MoveRectangle(ref Rectangle newRectangle, Rectangle oldRectangle)
-        {
-            oldRectangle.Intersect(newRectangle);
-            if (DirectionSpiral == Direction.Down)
-            {
-                newRectangle.X = newRectangle.X - oldRectangle.Width;
-                return;
-            }
-            if (DirectionSpiral == Direction.Rigth)
-            {
-                newRectangle.Y = newRectangle.Y + oldRectangle.Height;
-                return;
-            }
-
-            if (DirectionSpiral == Direction.Top)
-            {
-                newRectangle.X = newRectangle.X + oldRectangle.Width; 
-                return;
-            }
-
-            if (DirectionSpiral == Direction.Left)
-            {
-                newRectangle.Y = newRectangle.Y - oldRectangle.Height;
-            }
-        }
-
-
-        private void DeleteIntersection(ref Rectangle rectangle)
-        {
-            foreach (var buildRectangle in allBuildRectangles)
-            {
-                if (buildRectangle.IntersectsWith(rectangle))
-                {
-                    MoveRectangle(ref rectangle, buildRectangle);
-                    DeleteIntersection(ref rectangle);
-                }
-            }
-        }
-
-        private Rectangle BuildRectangle(Size rectangleSize)
-        {
-            if (allBuildRectangles.Count == 0)
-            {
-                return new Rectangle(center, rectangleSize);
-            }
-            var lastRectangle = allBuildRectangles.Last();
-            var deltaX = rectangleSize.Width;
-            var deltaY = lastRectangle.Height;
-            if (DirectionSpiral == Direction.Rigth)
-            {
-                deltaX = lastRectangle.Width;
-            }
-            if (DirectionSpiral == Direction.Top)
-            {
-                deltaY = rectangleSize.Height;
-            }
-
-            var locationNewRectangle = new Point()
-            {
-                X = deltaX * DirectionSpiral.X + lastRectangle.X,
-                Y = deltaY * DirectionSpiral.Y + lastRectangle.Y
-            };
-            var result = new Rectangle(locationNewRectangle, rectangleSize);
-            DeleteIntersection(ref result);
-            return result;
+            centerCloud = center;
+            BuiltRectangles = new List<Rectangle>();
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            var result = BuildRectangle(rectangleSize);
-            allBuildRectangles.Add(result);
-            AnalyzeLayout();
-            CountTags++;
-            return result;
-        }
-    }
 
-    [TestFixture]
-    class TestTagsCload
-    {
-        [Test]
-        public void BuildOneTegInCenter()
-        {
-            var pointCenter = new Point(5, 5);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var rectangleSize = new Size(10, 8);
-            var newTagsRectangle = circle.PutNextRectangle(rectangleSize);
-            Assert.AreEqual(new Rectangle(new Point(5, 5), rectangleSize), newTagsRectangle);
-        }
-
-        [Test]
-        public void BuildSquareSpiralLeftDirection()
-        {
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var rectangleSize = new Size(1, 1);
-            Assert.AreEqual(new Rectangle(new Point(10, 10), rectangleSize),
-                circle.PutNextRectangle(rectangleSize));
-            Assert.AreEqual(new Rectangle(new Point(9, 10), rectangleSize),
-                circle.PutNextRectangle(rectangleSize));
-        }
-
-        [Test]
-        public void BuildSquareSpiralLeftAndDownDirection()
-        {
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var rectangleSize = new Size(1, 1);
-            circle.PutNextRectangle(rectangleSize);
-            circle.PutNextRectangle(rectangleSize);
-
-            Assert.AreEqual(new Rectangle(new Point(9, 11), rectangleSize),
-                circle.PutNextRectangle(rectangleSize));
-
-        }
-
-        [Test]
-        public void BuildSquareSpiralLeftDownRight()
-        {
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var rectangleSize = new Size(1, 1);
-            circle.PutNextRectangle(rectangleSize);
-            circle.PutNextRectangle(rectangleSize);
-            circle.PutNextRectangle(rectangleSize);
-
-            Assert.AreEqual(new Rectangle(new Point(10, 11), rectangleSize),
-                circle.PutNextRectangle(rectangleSize));
-
-            Assert.AreEqual(new Rectangle(new Point(11, 11), rectangleSize),
-                circle.PutNextRectangle(rectangleSize));
-
-        }
-
-        [Test]
-        public void BuildFullSpiralWithSquareTag()
-        {
-            var expectedResult = new List<Point>()
+            while (true)
             {
-                new Point(10, 10),
-                new Point(9, 10),
-                new Point(9, 11),
-                new Point(10, 11),
-                new Point(11, 11),
-                new Point(11, 10),
-                new Point(11, 9),
-                new Point(10, 9),
-                new Point(9, 9),
-                new Point(8, 9)
-            };
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var rectangeSize = new Size(1, 1);
-            var actualList = new List<Point>();
-            for (var i = 0; i < 10; i++)
-            {
-                var rectangle = circle.PutNextRectangle(rectangeSize);
-                actualList.Add(new Point(rectangle.X, rectangle.Y));
+                var rectangle = new Rectangle(CalculateCenterNewRectangle(rectangleSize), rectangleSize);
+                if (!BuiltRectangles.Any(rec => rec.IntersectsWith(rectangle)))
+                {
+                    BuiltRectangles.Add(rectangle);
+                    break;
+                }
             }
-            actualList.Should().Equal(expectedResult);
+            return BuiltRectangles.Last();
         }
 
-        [Test]
-        public void BuildSpiralWithBigSquareInCenterAndLittleSquareAround()
+        private Point CalculateCenterNewRectangle(Size rectangleSize)
         {
+            step += 0.1;
+            var x = step * Math.Cos(step) + centerCloud.X + rectangleSize.Width / 2;
+            var y = step * Math.Sin(step) + centerCloud.Y + rectangleSize.Height / 2;
+            return new Point((int) x, (int) y);
 
-            var expectedResult = new List<Point>()
-            {
-                new Point(9, 10),
-                new Point(9, 11),
-                new Point(10, 13),
-                new Point(11, 13),
-                new Point(13, 12),
-                new Point(13, 11),
-                new Point(12, 9),
-                new Point(11, 9),
-                new Point(10, 9)
-            };
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var centerSqareSize = new Size(3,3);
-            circle.PutNextRectangle(centerSqareSize);
-            var littleSqareSize = new Size(1,1);
-            var actualList = new List<Point>();
-
-            for (int i = 0; i < 9; i++)
-            {
-                actualList.Add(circle.PutNextRectangle(littleSqareSize).Location);
-            }
-
-            actualList.Should().Equal(expectedResult);
         }
 
-        [Test]
-        public void BuildThreeDiferentRectangle()
-        {
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var firstFectangeSize = new Size(1, 1);
-            var secondFectangeSize = new Size(2, 2);
-            var thirdFectangeSize = new Size(2, 1);
-
-            circle.PutNextRectangle(firstFectangeSize);
-            circle.PutNextRectangle(secondFectangeSize);
-
-            Assert.AreEqual(new Rectangle(new Point(8, 12), thirdFectangeSize),
-                circle.PutNextRectangle(thirdFectangeSize));
-        }
-
-        [Test]
-        public void BuildFullSpiralDiferentRectangle()
-        {
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var r1 = new Size(1, 1);
-            var r2 = new Size(2, 2);
-            var r3 = new Size(1, 1);
-            var r4 = new Size(3, 1);
-            var r5 = new Size(2, 3);
-            var r6 = new Size(1, 2);
-            var r7 = new Size(2, 1);
-            var r8 = new Size(3, 2);
-            var r9 = new Size(1, 2);
-            circle.PutNextRectangle(r1);
-            circle.PutNextRectangle(r2);
-            circle.PutNextRectangle(r3);
-            Assert.AreEqual(new Rectangle(new Point(9, 12), r4),
-                circle.PutNextRectangle(r4));
-            Assert.AreEqual(new Rectangle(new Point(12, 12), r5),
-                circle.PutNextRectangle(r5));
-            Assert.AreEqual(new Rectangle(new Point(12, 10), r6),
-                circle.PutNextRectangle(r6));
-            Assert.AreEqual(new Rectangle(new Point(12, 9), r7),
-                circle.PutNextRectangle(r7));
-            Assert.AreEqual(new Rectangle(new Point(9, 8), r8),
-                circle.PutNextRectangle(r8));
-            Assert.AreEqual(new Rectangle(new Point(8, 8), r9),
-                circle.PutNextRectangle(r9));
-        }
-
-        [Test]
-        public void LargeIntersection()
-        {
-            var pointCenter = new Point(10, 10);
-            var circle = new CircularCloudLayouter(pointCenter);
-            var sizeCenterRectangle = new Size(3,3);
-            var sizeLittleRectangleAroundCenter = new Size(1, 1);
-            var sizeBigRectangle = new Size(6, 3);
-            circle.PutNextRectangle(sizeCenterRectangle);
-            for (int i = 0; i < 6; i++)
-            {
-                circle.PutNextRectangle(sizeLittleRectangleAroundCenter);
-            }
-            Assert.AreEqual(new Rectangle(new Point(7,7), sizeBigRectangle ),
-                circle.PutNextRectangle(sizeBigRectangle));
-        }
     }
 }
